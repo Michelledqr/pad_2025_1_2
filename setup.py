15df40e8500f299700fdd45a64c95919ee7b6572
Michelledqr <100885207+Michelledqr@users.noreply.github.com>
2025-05-11 06:00:57 +0000

    correction setup & gitingnore

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name="edu_pad",
     version="0.0.1",
-    author="Michelle Robinson"
-    authon_email="michelle.robinson@est.iudigital.edu.co"
-    description="Prog para análisis de datos"
+    author="Michelle Robinson",
+    authon_email="michelle.robinson@est.iudigital.edu.co",
+    description="Prog para análisis de datos",
     pymodules=["actividad1","actividad2"],
     install_requires=[
         "pandas",
