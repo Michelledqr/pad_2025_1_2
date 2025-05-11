03baf8c9a7dc2f0ff60a52a4ddf25b829ac8d0be
Michelledqr <100885207+Michelledqr@users.noreply.github.com>
2025-05-11 05:41:19 +0000

    empaquetado de dependencias

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="edu_pad",
+    version="0.0.1",
+    author="Michelle Robinson"
+    authon_email="michelle.robinson@est.iudigital.edu.co"
+    description="Prog para análisis de datos"
+    pymodules=["actividad1","actividad2"],
+    install_requires=[
+        "pandas",
+        "openpyxl",
+        "requests",
+        "beautifulsoup4"
+    ]
+)
